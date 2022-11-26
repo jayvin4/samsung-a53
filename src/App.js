67f68ea43d90ffcdad3b38954ac28bfc4625a961
jayvin4 <118872740+jayvin4@users.noreply.github.com>
2022-11-26 12:00:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React  from "react";
+import Feacture from "./pages/Feacture";
+import Specs from "./pages/Specs";
+import Footer from "./pages/Footer";
+import Support from "./pages/Support";
+import Compare from "./pages/Compare";
+import Navbar from "./pages/Navbar";
+import Where from "./pages/Wheretobuy";
+
+function App() {
+    return (
+      <> 
+ 
+        <Navbar />
+        <Feacture />
+        <Specs />
+        <Support />
+        <Compare />
+        <Footer />
+<Where />
+      </>
+     );
+};
+export default App;
